c8b3d4ee6651ee75e41e71c1582bf3ac406bd599
Mariusz Jacek <marios.yaceg@gmail.com>
2022-08-19 09:59:14 +0200

    Fix section

--- src/components/Section.js
+++ src/components/Section.js
@@ -4,13 +4,13 @@ import '../App.css'
 export default function Section(props){
     return(
 
-    <div id="sectionOrigin" className="secionOrigin">
+    <div id="sectionOrigin" className="sectionOrigin">
         <div className="sectionContent">
             <h2>{props.title}</h2>
             <p>{props.description}</p>
         </div>
         <div className="sectionImage">
-            <img src={`../images/${props.coverImg}`} alt="" id="coffeeImage"/>
+            <img src={`../images/${props.coverImg}`} alt="" id="coffeeImage" className="sectionOriginImage"/>
         </div>
         
         <div className="originHeaderContent"></div>
